7332164ebb2e76926a50038b15b82203088c425c
Lanisha Nicholas <88057783+LanishaNicholas@users.noreply.github.com>
2022-09-21 12:23:02 +0200

    Add files via upload
    bug fixes

--- booking/index.php
+++ booking/index.php
@@ -24,13 +24,14 @@
 <body>
     <div class="container-fluid">
         <!-- Carousel -->
-        <div id="demo" class="carousel slide" data-bs-ride="carousel">
+        <div id="slideShow" class="carousel slide" data-bs-ride="carousel">
 
             <!-- Indicators/dots -->
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-                <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-                <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+                <button type="button" data-bs-target="#slideShow" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#slideShow" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#slideShow" data-bs-slide-to="2"></button>
+                <button type="button" data-bs-target="#slideShow" data-bs-slide-to="3"></button>
             </div>
 
             <!-- The slideshow/carousel -->
@@ -48,13 +49,16 @@
                 <div class="carousel-item">
                   <img src="img/slide3.jpg" alt="New York" class="d-block w-100">
                 </div>
+                <div class="carousel-item">
+                  <img src="img/slide4.jpg" alt="New York" class="d-block w-100">
+                </div>
             </div>
 
             <!-- Left and right controls/icons -->
-            <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#slideShow" data-bs-slide="prev">
             <span class="carousel-control-prev-icon"></span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#slideShow" data-bs-slide="next">
                 <span class="carousel-control-next-icon"></span>
             </button>
             <!-- about the slide -->
@@ -80,13 +84,13 @@
                             <a class="nav-link" href="view/doctorAccount.php">Doctor</a>
                         </li>
                         <li class="nav-item">
-                            <button type="button" class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#demo">Book Appointment</button>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#bookAppointment">Book Appointment</button>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-        <div class="main collapse" id="demo">
+        <div class="main collapse" id="bookAppointment">
             
              <form>
                 <table>
